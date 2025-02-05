aceb1b6829ab25b163b588d7b551212134912d4b
André Räubig <129953354+araeubig@users.noreply.github.com>
2025-02-05 08:37:31 +0000

    Added flightphp files

---
+++ API/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Wenn Sie Composer verwenden, erfordern Sie den Autoloader.
+require 'vendor/autoload.php';
+// Wenn Sie Composer nicht verwenden, laden Sie das Framework direkt
+// require 'flight/Flight.php';
+
+// Definieren Sie dann eine Route und weisen Sie eine Funktion zur Behandlung der Anfrage zu.
+Flight::route('/', function () {
+  echo 'Hallo Welt!';
+});
+
+// Starten Sie das Framework schließlich.
+Flight::start();
---
+++ API/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/vip-go-skeleton',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1b7de63c4f211f6234c24a6862bd78f4d33387a8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'automattic/vip-go-skeleton' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1b7de63c4f211f6234c24a6862bd78f4d33387a8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'flightphp/core' => array(
+            'pretty_version' => 'v1.3.9',
+            'version' => '1.3.9.0',
+            'reference' => 'bc5e03ca59accb3ef37e31b6d4a2a9c78c7bb7ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../flightphp/core',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
