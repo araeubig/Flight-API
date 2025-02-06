2055445885742419679bf5b1ac3e7030c6574726
André Räubig <129953354+araeubig@users.noreply.github.com>
2025-02-06 05:30:17 +0000

    Added routes

--- API/routes/api.php
+++ API/routes/api.php
@@ -3,8 +3,14 @@
 use App\Controllers\UserController;
 
 Flight::route('GET /', [new UserController(), 'hello']);
-Flight::route('GET /users', [new UserController(), 'index']);
-Flight::route('GET /users/@id', [new UserController(), 'show']);
-Flight::route('POST /users', [new UserController(), 'store']);
-Flight::route('PUT /users/@id', [new UserController(), 'update']);
-Flight::route('DELETE /users/@id', [new UserController(), 'delete']);
+//Flight::route('GET /state', [new UserController(), 'hello']);
+// Flight::route('POST /state', [new UserController(), 'store']);
+// Flight::route('GET /open', [new UserController(), 'hello']);
+// Flight::route('POST /github', [new UserController(), 'store']);
+
+
+// Flight::route('GET /users', [new UserController(), 'index']);
+// Flight::route('GET /users/@id', [new UserController(), 'show']);
+// Flight::route('POST /users', [new UserController(), 'store']);
+// Flight::route('PUT /users/@id', [new UserController(), 'update']);
+// Flight::route('DELETE /users/@id', [new UserController(), 'delete']);
