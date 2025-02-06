3d3eec2a5b56d995f213919d6022f8bbb063bb8e
André Räubig <129953354+araeubig@users.noreply.github.com>
2025-02-06 11:26:12 +0000

    new config

--- .devcontainer/features/phpmyadmin/config.inc.php
+++ .devcontainer/features/phpmyadmin/config.inc.php
@@ -9,9 +9,10 @@ $i++;
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'wordpress';
-$cfg['Servers'][$i]['password'] = 'wordpress';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = '';
 /* $cfg['Servers'][$i]['only_db'] = 'wordpress'; */
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
